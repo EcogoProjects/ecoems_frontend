d91ca8c8f79d09983466a4a06bf79411fb44db30
Wich0D <luheco1c@gmail.com>
2026-04-11 13:10:09 -0600

    Añadiendo vercel analytics

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-# Ecoems App frontend
+# Ecogo App frontend
 
 
 ## Instalación 
 
 Instala las dependencias:
 
-Las siguientes dependencias pertenences a los iconos utilizados en la app
+Las siguientes dependencias pertenencen a los iconos utilizados en la app:
 ```bash
 npm install react-icons
 npm install @boxicons/react
+```
+
+Las siguientes dependecias pertenecen a las análiticas de vercel:
+```bash
+npm i @vercel/analytics
 ```
--- ecoems-landing/src/app/layout.tsx
+++ ecoems-landing/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import {Analytics} from '@vercel/analytics/next'
 import './globals.css'
 import Providers from './providers'
 import FaviconSwitcher from '@/components/shared/FaviconSwitcher'
@@ -46,6 +47,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <GoogleAnalytics />
         <FaviconSwitcher />
         <Providers>{children}</Providers>
+        <Analytics/>
       </body>
     </html>
   )
